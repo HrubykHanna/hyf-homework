74d5ce8984c150f25f4e100c171f4adfd82700f0
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2023-03-03 22:08:09 +0100

    Price estimator

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -23,3 +23,43 @@ if (shouldShowResultInDogYears) {
 else {
     console.log('Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture);
 }
+
+// Housey pricey (A house price estimator)
+
+
+// Peter Hous
+
+const petersWide = 8;
+const petersDeep = 10;
+const petersHigh = 10;
+const peterGardenSizeInM2 = 100,
+
+const volumeInMeters = petersWide * petersDeep * petersHigh;
+const housePrice = volumeInMeters * 2.5 * 1000 + peterGardenSizeInM2 * 300;
+
+const peterHousCost = 2.500.000;
+
+if (housePrice < peterHousCost)
+    console.log('Peter will pay too little');
+else (housePrice > peterHousCost)
+console.log('Peter will pay too much');
+
+
+// Julia Hous
+
+const juliasWide = 8;
+const juliasDeep = 10;
+const juliasHigh = 10;
+const juliasGardenSizeInM2 = 100,
+
+const volumeInMeters = juliasWide * juliasDeep * juliasHigh;
+const housePrice = volumeInMeters * 2.5 * 1000 + juliasGardenSizeInM2 * 300;
+
+const juliasHousCost = 2.500.000;
+
+if (housePrice < juliasHousCost)
+    console.log('julia will pay too little');
+else (housePrice > juliasHousCost)
+console.log('Julia will pay too much');
+
+
