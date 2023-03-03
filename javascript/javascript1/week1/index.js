d0cabf987446f3ca23e7ac6c20d230be845245e5
HackYourFutureDK <hackyourfuturedk@HackYourFutureDKs-MacBook-Pro.local>
2023-03-03 21:02:55 +0100

    dog_age_calculator

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -8,3 +8,18 @@ const yearFuture = 2050;
 const age = yearFuture - yearOfBirth;
 
 console.log('You will be ' + age + ' years old in ' + yearFuture);
+
+
+// Goodboy-Oldboy (A dog age calculator) 
+
+const dogYearOfBirth = 2020;
+const dogYearFuture = 2050;
+const dogYear = dogYearFuture - dogYearOfBirth;
+let shouldShowResultInDogYears = true
+
+if (shouldShowResultInDogYears) {
+    console.log('Your dog will be ' + dogYear * 7 + ' dog years old in ' + dogYearFuture);
+}
+else {
+    console.log('Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture);
+}
